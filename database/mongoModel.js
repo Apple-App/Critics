afefcf52160b6239cff5c8a962732e0a33813024
Sujin Lee <slee1016@gmail.com>
2019-01-12 11:46:27 -0500

    create mongoose model

--- database/mongoModel.js
+++ database/mongoModel.js
@@ -1,3 +1,25 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/badapples-reviews');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/badapples-reviews');
 
+const criticSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  topCritic: Number,
+  publisher: String,
+  picture: String
+});
+
+const reviewSchema = mongoose.Schema({
+  id: Number,
+  criticId: Number,
+  text: String,
+  rating: Number,
+  movieId: Number,
+  date: String
+});
+
+const Critic = mongoose.model('critic', criticSchema);
+const Review = mongoose.model('review', reviewSchema);
+
+module.exports = Critic;
+module.exports = Review;
